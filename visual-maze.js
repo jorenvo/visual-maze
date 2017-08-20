1342d94186f58e720f7b597bdb3a86e99e8f4e73
Joren Van Onder <jov@odoo.com>
2017-08-20 09:28:05 -0700

    implement basic solver and draw solution

--- visual-maze.js
+++ visual-maze.js
@@ -26,11 +26,12 @@ class Maze {
             image.src = image_name;
             image.onload = () => {
                 context.drawImage(image, 0, 0);
-                this.maze_width = image.width;
-                this.maze_height = image.height;
+                this.width = image.width;
+                this.height = image.height;
                 let image_data = context.getImageData(0, 0, image.width, image.height);
                 this._initSquaresFromImageData(image_data);
                 this._validateState();
+                this.context.scale(this.canvas.width / this.width, this.canvas.height / this.height);
                 resolve();
             };
         });
@@ -44,9 +45,9 @@ class Maze {
                 new_square.initFromRGBA(...rgba);
 
                 if (new_square.type === 'entrance') {
-                    this.entrance = new Position(Math.floor(this.square.length / this.maze_width), this.square.length % this.maze_height);
+                    this.entrance = new Position(this.square.length % this.width, Math.floor(this.square.length / this.width));
                 } else if (new_square.type === 'exit') {
-                    this.exit = new Position(Math.floor(this.square.length / this.maze_width), this.square.length % this.maze_height);
+                    this.exit = new Position(this.square.length % this.height, Math.floor(this.square.length / this.width));
                 }
 
                 this.square.push(new_square);
@@ -59,20 +60,36 @@ class Maze {
         }
     }
 
-    getSquare (column, row) {
-        return this.square[row * this.maze_width + column];
+    isInBounds (position) {
+        if (! (position.row >= 0 && position.row < this.height)) {
+            return false;
+        } else if (! (position.column >= 0 && position.column < this.width)) {
+            return false;
+        }
+        return true;
+    }
+
+    hasToBeHandled (position) {
+        if (! this.isInBounds(position)) {
+            return false;
+        }
+        return this.getSquare(position).hasToBeHandled();
+    }
+
+    getSquare (position) {
+        return this.square[position.row * this.width + position.column];
     }
 
     draw () {
-       this.initialized.then(() => {
-            this.context.scale(this.canvas.width / this.maze_width, this.canvas.height / this.maze_height);
-            for (let row = 0; row < this.maze_height; ++row) {
-                for (let column = 0; column < this.maze_width; ++column) {
-                    this.context.fillStyle = this.getSquare(column, row).toRGB();
+        return this.initialized.then(() => {
+            for (let row = 0; row < this.height; ++row) {
+                for (let column = 0; column < this.width; ++column) {
+                    let position = new Position(column, row);
+                    this.context.fillStyle = this.getSquare(position).toRGB();
                     this.context.fillRect(column, row, 1, 1);
                 }
             }
-       });
+        });
     }
 }
 
@@ -96,16 +113,92 @@ class Square {
             exit: 'rgb(255, 255, 255)',
             wall: 'rgb(0, 0, 0)',
             solution: 'rgb(255, 0, 0)',
+            traversed: 'rgb(100, 100, 100)',
         };
 
         return TYPE2RGB[this.type];
     }
+
+    hasToBeHandled () {
+        return this.type === 'path' || this.type === 'entrance' || this.type === 'exit';
+    }
 }
 
 class Position {
-    constructor (row, column) {
-        this.row = row;
+    constructor (column, row) {
         this.column = column;
+        this.row = row;
+    }
+
+    equals (other) {
+        return this.column === other.column && this.row === other.row;
+    }
+
+    getEast () {
+        return new Position(this.column + 1, this.row);
+    }
+
+    getSouth () {
+        return new Position(this.column, this.row + 1);
+    }
+
+    getWest () {
+        return new Position(this.column - 1, this.row);
+    }
+
+    getNorth () {
+        return new Position(this.column, this.row - 1);
+    }
+}
+
+class Solver {
+    constructor (maze) {
+        this.maze = maze;
+        this.iteration = [];
+        this.path = [];
+    }
+
+    solve () {
+        console.error('Not implemented');
+    }
+}
+
+class DFSSolver extends Solver {
+    solve () {
+        let found_exit = this.findExit(this.maze.entrance);
+        this.markSolution();
+        return found_exit;
+    }
+
+    findExit (position) {
+        if (! this.maze.hasToBeHandled(position)) {
+            return false;
+        }
+
+        this.path.push(position);
+
+        if (position.equals(this.maze.exit)) {
+            return true;
+        }
+
+        this.maze.getSquare(position).type = 'traversed';
+
+        if (this.findExit(position.getEast())) {
+            return true;
+        } else if (this.findExit(position.getSouth())) {
+            return true;
+        } else if (this.findExit(position.getWest())) {
+            return true;
+        } else if (this.findExit(position.getNorth())) {
+            return true;
+        }
+
+        this.path.pop();
+        return false;
+    }
+
+    markSolution () {
+        this.path.forEach((position) => this.maze.getSquare(position).type = 'solution');
     }
 }
 
@@ -122,15 +215,31 @@ function initUI () {
     _initButtonGroup('maze-selection');
 }
 
+function _displayMessage (id, message) {
+    let element = document.getElementById(id);
+    element.innerHTML = message;
+    element.classList.remove('hide');
+}
+
+function displayInfo (message) {
+    _displayMessage('info', message);
+}
+
 function displayWarning (message) {
-    let warning = document.getElementById('warning');
-    warning.innerHTML = message;
-    warning.classList.remove('hide');
+    _displayMessage('warning', message);
 }
 
 function init () {
     let maze = new Maze('maze', 'big.png');
-    maze.draw();
+    maze.draw().then(() => {
+        let solver = new DFSSolver(maze);
+        if (solver.solve()) {
+            displayInfo('Found solution in TODO seconds.');
+        } else {
+            displayInfo('Determined maze had no solution in TODO seconds.');
+        }
+        maze.draw();
+    });
 
     initUI();
 }
